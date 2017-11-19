c16e7e2189a0f04cae53c2e8b28f300a129e6629
Pazu <libelulaby@gmail.com>
2017-11-18 19:08:27 -0500

    Finishing development and functionalities

--- src/js/app.js
+++ src/js/app.js
@@ -4,92 +4,97 @@ import '../scss/styles.scss'
 (function(){
 
 document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
+  	if (document.readyState === 'complete') {
 
-  	document.getElementById("background").onclick = hideBackground;
-	document.getElementById("ic-toggle").onclick = openNav; 
-	document.querySelector('.link-interest').onclick = smothScroll;
+  		smothScroll();
+  		document.getElementById("background").onclick = hideSidenav;
+		document.getElementById("ic-toggle").onclick = openNav;
 
-// Desktop and mobile navigation events
+  		function smothScroll() {
+  			if (window.innerWidth < 768) {
+				var mainMenu = document.querySelector('.menu-xs');
+				console.log(mainMenu);	 	
+			} else {
+  				var mainMenu = document.querySelector('.menu');
+  			}
+  			for (var i = 0; i < mainMenu.children.length; i++) {
+	  			var item = mainMenu.children[i];
+	  			item.addEventListener('click', scrollHandler(item.className))
+  			}
+  		}
 
-	let items = document.getElementsByClassName('menu-a');
-  	let submenu = document.getElementsByClassName('submenu');
-  	
-  	let sideitems = document.getElementsByClassName('link-subxs');
-  	let sidesubmenu = document.getElementsByClassName('submenuxs');  	
+	  	function scrollHandler (section) {
+	  		return function () {
+	  			var selector = "."+section+"-content";
+	  			if (window.innerWidth < 768) {
+					hideSidenav();	 	
+				}	  			
+	  			document.querySelector(selector).scrollIntoView({ 
+				  behavior: 'smooth' 
+				});
+	  		}
+	  	}
 
-  	for ( var i = 0; i < sideitems.length; i++ ) (	
-		function showSubmenu(i){ 
-			sideitems[i].onclick = function() {
-				let active = "menu-a link-subxs";
-				let arrow = "url(img/ic_arrow.svg)";
-				let display = "none";
-
-				if (!(sideitems[i].className.match("active"))) { 
-					active = "active menu-a link-subxs";
-					arrow = "url(img/ic_arrow_up.svg)";
-					display = "block";
-				}
-
-				sideitems[i].className = active;
-				sideitems[i].style.backgroundImage = arrow;
-				sidesubmenu[i].style.display = display				
+		var animateHTML = function () {
+			var elems,
+			    windowHeight
+			 var init = function () {
+			    elems = document.getElementsByClassName('hidden')
+			    windowHeight = window.innerHeight
+			    _addEventHandlers()
+			 }
+		  	var _addEventHandlers = function () {
+			    window.addEventListener('scroll', _checkPosition)
+			    window.addEventListener('resize', init)
+			  }
+			var _checkPosition = function () {
+			    for (var i = 0; i < elems.length; i++) {
+			      	var posFromTop = elems[i].getBoundingClientRect().top
+			      	if (posFromTop - windowHeight <= 0) {
+			        	elems[i].className = elems[i].className.replace('hidden', 'fade-in-element')
+			      	}
+			    }
 			}
-
-			items[i].onclick = function (){ 
-				let display = "none";
-				if (!(submenu[i].className.match("active"))) { 
-					display = "block";  			
-				}
-				document.getElementById("background").style.display = display;
-			}				  
+			return {
+			    init: init
+			}
 		}
-	)(i); /* Close the loop */ 
-
-function smothScroll() {
-	document.querySelector('.interest').scrollIntoView({ 
-	  behavior: 'smooth' 
-	});
-}
-
-function hideBackground() {		
-	if (window.innerWidth < 768) {
-		hideSidenav ();	 	
-	}
-	document.getElementById("background").style.display = "none";
-}
-
-
+		animateHTML().init()
      
-function openNav() {
-	if (!(document.getElementById("ic-toggle").className.match("close"))) { 
-
-		document.getElementById("ic-toggle").style.backgroundImage = "url(img/toggle-close.svg)";
-		var w = window.innerWidth; 
-		var marginl = w-72; //72px is the id content's width, when the sidenav appears
-	    document.getElementById("content").style.marginLeft = marginl+"px"; //Set the #content's margin-left
-	    document.getElementById("hugenav").style.width = marginl+"px"; //Set the #hugenav's width
-
-	    document.body.style.overflow = "hidden";
-		document.getElementById("ic-toggle").className = "close";
-		document.getElementById("background").style.display = "block"; 
-	}
-
-	else { hideSidenav ();}
-}
-
-function hideSidenav(){
-	document.getElementById("hugenav").style.width = "0";
-    document.getElementById("content").style.marginLeft = "0";	    
-    document.body.style.overflow = "visible";
-    document.getElementById("ic-toggle").style.backgroundImage = "url(img/toggle-open.svg)";
-	document.getElementById("ic-toggle").className = ""; 
-	document.getElementById("background").style.display = "none";
-}
+		function openNav() {
+			var toggleIcon = document.getElementById("ic-toggle");
 
 
-} /* Close readystate */
+			if (!(toggleIcon.className.match("close"))) { 
+				var w = window.innerWidth; 
+				var marginl = w-72;
 
+				document.querySelector('#ic-toggle .fa-bars').style.display = "none";
+				document.querySelector('#ic-toggle .fa-times').style.display = "inline-block";
+
+				toggleIcon.className = "close";
+				
+			    document.getElementById("content").style.marginLeft = marginl+"px"; //Set the #content's margin-left
+			    document.getElementById("hugenav").style.width = marginl+"px"; //Set the #hugenav's width
+			    document.getElementById("background").style.display = "block"; 
+
+			    document.body.style.overflow = "hidden";		
+				
+			} else { 
+				hideSidenav ();
+			}
+		}
+
+		function hideSidenav(){
+			document.getElementById("hugenav").style.width = "0";
+		    document.getElementById("content").style.marginLeft = "0";	    
+		    document.body.style.overflow = "visible";
+		    document.querySelector('#ic-toggle .fa-bars').style.display = "inline-block";
+			document.querySelector('#ic-toggle .fa-times').style.display = "none";
+			document.getElementById("ic-toggle").className = ""; 
+			document.getElementById("background").style.display = "none";
+		}
+	} 
 } /* Close onready */
   
 })();
